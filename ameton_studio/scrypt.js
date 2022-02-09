f0ef01222fe975bcb5235622b5a3d71bb2489897
17clouds <92887957+17clouds@users.noreply.github.com>
2022-02-09 23:37:53 +0300

    Update scrypt.js

--- ameton_studio/scrypt.js
+++ ameton_studio/scrypt.js
@@ -1,24 +1,17 @@
-
-let position = 0;
-const slidesToShow = 4;
-const slidesToScroll = 1;
-const container = document.querySelector('.questions_carousel')
-const track = document.querySelector('.questions_tiles')
-// const item = document.querySelector('.tile')
-const btnPrev = document.querySelector('.btn_prev')
-const btnNext = document.querySelector('.btn_next')
-const items = document.querySelectorAll('.tile')
-const itemsCount = items.length;
-const itemWidth = container.clientWidth / slidesToShow;
-const movePosition = slidesToScroll * itemWidth;
-
-// items.forEach((item) => {
-//    item.style.minWidth =  `${itemWidth}px`
-// })
-
+var position = 0;
+var slidesToShow = 4;
+var slidesToScroll = 1;
+var container = document.querySelector('.questions_carousel')
+var track = document.querySelector('.questions_tiles')
+var btnPrev = document.querySelector('.btn_prev')
+var btnNext = document.querySelector('.btn_next')
+var items = document.querySelectorAll('.tile')
+var itemsCount = items.length;
+var itemWidth = container.clientWidth / slidesToShow;
+var movePosition = slidesToScroll * itemWidth;
 
 btnNext.addEventListener('click', () => {
-   const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+   var itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
 
    position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
 
@@ -27,7 +20,7 @@ btnNext.addEventListener('click', () => {
 });
 
 btnPrev.addEventListener('click', () => {
-   const itemsLeft = Math.abs(position) / itemWidth;
+   var itemsLeft = Math.abs(position) / itemWidth;
 
    position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
 
@@ -35,45 +28,13 @@ btnPrev.addEventListener('click', () => {
    checkBtns();
 });
 
-const setPosition = () => {
+var setPosition = () => {
    track.style.transform = `translateX(${position}px)`
 };
 
-const checkBtns = () => {
+var checkBtns = () => {
    btnPrev.disabled = position === 0;
    btnNext.disabled = position <= - (itemsCount - slidesToShow) * itemWidth;
 };
 
 checkBtns();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let width = 306;
-// let count = 1;
-// let carousel = document.querySelector('.questions_carousel')
-
-// let list = carousel.querySelector('.questions_tiles');
-// let listElems = carousel.querySelectorAll('.tile');
-
-// let position = 0;
-
-
-
-// carousel.querySelector('.questions_btn').onclick = function () {
-//    position -= width * count;
-//    position = Math.max(position, -width * (listElems.length - count));
-//    list.style.marginLeft = position + 'px';
-// };
-
