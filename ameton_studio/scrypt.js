49ec5b232703ad06ef4d23bc4c131746a2687da8
17clouds <92887957+17clouds@users.noreply.github.com>
2022-02-09 23:45:26 +0300

    Update scrypt.js

--- ameton_studio/scrypt.js
+++ ameton_studio/scrypt.js
@@ -2,16 +2,13 @@ var position = 0;
 var slidesToShow = 4;
 var slidesToScroll = 1;
 var container = document.querySelector('.questions_carousel');
-var track = document.querySelector('.questions_tiles'); // const item = document.querySelector('.tile')
-
+var track = document.querySelector('.questions_tiles');
 var btnPrev = document.querySelector('.btn_prev');
 var btnNext = document.querySelector('.btn_next');
 var items = document.querySelectorAll('.tile');
 var itemsCount = items.length;
 var itemWidth = container.clientWidth / slidesToShow;
-var movePosition = slidesToScroll * itemWidth; // items.forEach((item) => {
-//    item.style.minWidth =  ${itemWidth}px
-// })
+var movePosition = slidesToScroll * itemWidth; 
 
 btnNext.addEventListener('click', function () {
   var itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
